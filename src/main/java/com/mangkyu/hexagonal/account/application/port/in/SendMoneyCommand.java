9a14def31fca0a0d41fa4e63954b7bf36506ee0a
MangKyu <whalsrb1226@naver.com>
2022-05-07 16:22:50 +0900

    생성자의 힘

--- src/main/java/com/mangkyu/hexagonal/account/application/port/in/SendMoneyCommand.java
+++ src/main/java/com/mangkyu/hexagonal/account/application/port/in/SendMoneyCommand.java
@@ -2,12 +2,14 @@ package com.mangkyu.hexagonal.account.application.port.in;
 
 import com.mangkyu.hexagonal.account.domain.AccountId;
 import com.mangkyu.hexagonal.account.domain.Money;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 
 @Getter
+@Builder
 @RequiredArgsConstructor
 public class SendMoneyCommand {
 
