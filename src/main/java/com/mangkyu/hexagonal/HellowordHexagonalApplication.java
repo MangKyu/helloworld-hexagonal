8d28dfc1b7b0cd3a20a12499769d3f6c689508c8
MangKyu <whalsrb1226@naver.com>
2022-05-07 15:00:40 +0900

    Initial Commit

---
+++ src/main/java/com/mangkyu/hexagonal/HellowordHexagonalApplication.java
@@ -0,0 +1,13 @@
+package com.mangkyu.hexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellowordHexagonalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HellowordHexagonalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mangkyu/hexagonal/HellowordHexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mangkyu.hexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellowordHexagonalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
