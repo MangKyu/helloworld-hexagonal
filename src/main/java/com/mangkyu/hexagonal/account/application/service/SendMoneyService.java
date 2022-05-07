44db5ee912411500517dbf43f2379d5a0aa82fe2
MangKyu <whalsrb1226@naver.com>
2022-05-07 16:50:57 +0900

    비즈니스 규칙 검증하기

--- src/main/java/com/mangkyu/hexagonal/account/application/service/SendMoneyService.java
+++ src/main/java/com/mangkyu/hexagonal/account/application/service/SendMoneyService.java
@@ -5,9 +5,13 @@ import com.mangkyu.hexagonal.account.application.port.in.SendMoneyUserCase;
 import com.mangkyu.hexagonal.account.application.port.out.AccountLock;
 import com.mangkyu.hexagonal.account.application.port.out.LoadAccountPort;
 import com.mangkyu.hexagonal.account.application.port.out.UpdateAccountStatePort;
+import com.mangkyu.hexagonal.account.domain.Account;
+import com.mangkyu.hexagonal.account.domain.AccountId;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
+
+import java.time.LocalDateTime;
 
 @Validated
 @Transactional
@@ -19,11 +23,20 @@ public class SendMoneyService implements SendMoneyUserCase {
     private final UpdateAccountStatePort updateAccountStatePort;
 
     @Override
-    public boolean sendMoney(final SendMoneyCommand sendMoneyCommand) {
+    public boolean sendMoney(SendMoneyCommand sendMoneyCommand) {
+        requireAccountExists(sendMoneyCommand.getSourceAccountId());
+        requireAccountExists(sendMoneyCommand.getTargetAccountId());
         // TODO: 비즈니스 규칙 검증
         // TODO: 모델 상태 조작
         // TODO: 출력값 반환
         return false;
+    }
+
+    private void requireAccountExists(AccountId targetAccountId) {
+        Account account = loadAccountPort.loadAccount(targetAccountId, LocalDateTime.now());
+        if (account == null) {
+            throw new IllegalArgumentException("Account not exists");
+        }
     }
 
 }
